96f87cd0ee5419d1e4ac43cd861b0fe75fb23696
Isadora Dantas <isadoramariadasilvadantas@gmail.com>
2023-03-24 18:40:56 -0300

    Add o Template

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module TodoRails
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.delete_method = :delete
+
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
